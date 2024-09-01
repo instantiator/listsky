9a4c8d0210eff4aad7d5ca8795542f8aa7690cab
Lewis Westbury <lewis@cantab.net>
2024-09-01 13:07:24 +0100

    Add subtitle

--- ListSky.Lib/DTO/AllLists.cs
+++ ListSky.Lib/DTO/AllLists.cs
@@ -3,6 +3,7 @@ namespace ListSky.Lib.DTO;
 public class AllLists
 {
     public string Title { get; set; } = null!;
+    public string SubTitle { get; set; } = null!;
     public IEnumerable<string> Descriptions { get; set; } = null!;
     public IEnumerable<ListMetadata> Lists { get; set; } = new List<ListMetadata>();
 }
