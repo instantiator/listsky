2695ec9cb4cb11ecaf1f11905fe29146ac7c31df
Lewis Westbury <lewis@cantab.net>
2024-09-21 20:37:00 +0100

    Prefix @ to mentions when composing update messages

--- ListSky.Lib/BlueSky/Composition/MessageComposer.cs
+++ ListSky.Lib/BlueSky/Composition/MessageComposer.cs
@@ -19,16 +19,15 @@ public class MessageComposer(ATConnection connection)
 
     public async Task<Tuple<string, IEnumerable<Facet>>> ComposeAsync(Composition composition, Dictionary<string,string> values)
     {
-        // $"➕ Added {addition.Name}, @{addition.AccountName_BlueSky} to list: {listResolution.Key.Title}";
         var message = composition switch
         {
-            Composition.AddedPersonToList => $"Added {values["account"]} to list: {values["listName"]}",
+            Composition.AddedPersonToList => $"Added @{values["account"]} to list: {values["listName"]}",
             _ => JsonSerializer.Serialize(values)
         };
 
         var facets = await GetAutoFacetsAsync(message);
 
-        // assume the list name appears in the message, and add a link to the list
+        // assume the list name appears at the end of the message after "list: ", and add a link to the list
         var listStartIndex = message.IndexOf("list: ") + 6;
         var listEndIndex = message.Length;
         var listUrlFacet = new Facet(new FacetIndex(listStartIndex, listEndIndex), FacetFeature.CreateLink(values["listUrl"]));
